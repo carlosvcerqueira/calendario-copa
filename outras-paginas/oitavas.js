57c279839b3585362911706c4e1ec459191a9bd3
carlosvcerqueira <carlosvcerqueira@gmail.com>
2022-11-04 13:44:27 -0300

    uptade calendar

---
+++ outras-paginas/oitavas.js
@@ -0,0 +1,48 @@
+function createGame(player1, hour, player2) {
+  return `
+    <li>
+        <img src="/assets/icons/icon-${player1}.svg" alt="Bandeira da ${player1}">
+        <strong>${hour}</strong>
+        <img src="/assets/icons/icon-${player2}.svg" alt="Bandeira de ${player2}">
+    </li>
+  `
+}
+
+let delay = -0.4
+function createCard(date, day, games) {
+  delay = delay + 0.4
+  return `
+    <div class="card" style="animation-delay: ${delay}s">
+          <h2>${date}<span>${day}</span></h2>
+          <ul>
+              ${games}
+          </ul>
+    </div>
+  `
+}
+
+document.querySelector("#cards").innerHTML =
+  createCard(
+    "03/12",
+    "sábado",
+    createGame("default", "12:00", "default") +
+      createGame("default", "16:00", "default")
+  ) +
+  createCard(
+    "04/12",
+    "domingo",
+    createGame("default", "12:00", "default") +
+      createGame("default", "16:00", "default")
+  ) +
+  createCard(
+    "05/12",
+    "segunda",
+    createGame("default", "12:00", "default") +
+      createGame("default", "16:00", "default")
+  ) +
+  createCard(
+    "06/12",
+    "terça",
+    createGame("default", "12:00", "default") +
+      createGame("default", "16:00", "default")
+  )
---
+++ outras-paginas/semifinais.js
@@ -0,0 +1,34 @@
+function createGame(player1, hour, player2) {
+  return `
+    <li>
+        <img src="/assets/icons/icon-${player1}.svg" alt="Bandeira da ${player1}">
+        <strong>${hour}</strong>
+        <img src="/assets/icons/icon-${player2}.svg" alt="Bandeira de ${player2}">
+    </li>
+  `
+}
+
+let delay = -0.4
+function createCard(date, day, games) {
+  delay = delay + 0.4
+  return `
+    <div class="card" style="animation-delay: ${delay}s">
+          <h2>${date}<span>${day}</span></h2>
+          <ul>
+              ${games}
+          </ul>
+    </div>
+  `
+}
+
+document.querySelector("#cards").innerHTML =
+  createCard(
+    "13/12",
+    "terça",
+    createGame("default", "16:00", "default")
+  ) +
+  createCard(
+    "14/12",
+    "quarta",
+    createGame("default", "16:00", "default")
+  )
